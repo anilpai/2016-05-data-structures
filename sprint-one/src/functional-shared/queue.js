4cd59549136e0789ec6093228e4b37efd6c227b6
Anil Pai <anilbpai@gmail.com>
2016-05-25 14:26:18 -0400

    Pair Programming with Rajkumar. 05/25

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    count : 0,
+    storage : {}
 };
 
-var queueMethods = {};
+_.extend(someInstance, queueMethods);
 
+return someInstance;
+};
 
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue : function(){
+    if(this.count){
+      this.count--;
+    }
+    var temp = this.storage[0];
+    delete this.storage[0];
+
+    for(var i=0; i<this.count;i++){
+      this.storage[i] =  this.storage[i+1];
+    }
+    return temp;
+  },
+  size: function(){
+    return this.count;
+  }
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count  = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function(){
+    if(count){
+      count--;
+    }
+    var temp = storage[0];
+    delete storage[0];
+
+    for(var i=0; i<count;i++){
+      storage[i] =  storage[i+1];
+    }
+    return temp;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
