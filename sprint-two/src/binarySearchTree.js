a2819b7ce88151a4859308cf86ecb55c5d577e69
Anil Pai <anilbpai@gmail.com>
2016-05-26 17:36:47 -0400

    Implemented Set, Hash Table & Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,69 @@
 var BinarySearchTree = function(value){
+
+  var binaryTree = {};
+  binaryTree.left = null;
+  binaryTree.right = null;
+  binaryTree.value = value;
+
+  _.extend(binaryTree, binaryTreeMethods);
+
+  return binaryTree;
 };
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value){
+  if(value < this.value){
+    if(this.left === null){
+      this.left = BinarySearchTree(value);
+    }else{
+      this.left.insert(value);
+    }
+  }else{
+    if(this.right === null){
+      this.right = BinarySearchTree(value);
+    }else{
+      this.right.insert(value);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(value){
+  if(this.value === value){
+    return true;
+  }
+  if(value < this.value){
+    if(this.left !== null){
+      return this.left.contains(value);
+    }else{
+      return false;
+    }
+  }else{
+    if(this.right !== null){
+      return this.right.contains(value);
+    }else{
+      return false;
+    }
+  }
+
+};
+
+binaryTreeMethods.depthFirstLog = function(callback){
+  callback(this.value);
+  if(this.left !== null){
+    this.left.depthFirstLog(callback);
+  }
+  if(this.right !== null){
+    this.right.depthFirstLog(callback);
+  }
+};
+
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Insert: O(log N)
+ * Contains : O(log N)
+ * depthFirstLog: O(N)
  */
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,19 +5,44 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k,v];
+  if(this._storage[i] === undefined){
+    this._storage[i] = [];
+  }
+  this._storage[i].push(tuple);
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var value = null;
+  if(this._storage[i] === undefined){
+    return value;
+  }
+  _.each(this._storage[i], function(item){
+    if(item[0] === k){
+      value = item[1];
+    }
+  });
+  return value;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = null;
 
+  _.each(this._storage[i], function(item, index){
+      if(item[0] === k){
+      this._storage[i].splice(index, 1);
+    }
+  });
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Insert : O(1)
+ * Retrieve : O(1)
+ * Remove : O(1)
  */
